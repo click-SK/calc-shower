cae83cdf7d2fd5b7e32700e9c5d0bae7053e258f
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-04-23 22:23:26 +0300

    update'

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
-import * as ShowerCabinController from "./controller/showerCabinController.js";
-import * as MirrorsStandartController from "./controller/MirrorsStandartController.js";
+import * as ShowerCabinController from './controller/showerCabinController.js';
+import * as MirrorsStandartController from './controller/MirrorsStandartController.js';
 
 dotenv.config();
 const app = express();
-const db = "mongodb+srv://roskichuk:qwerty12345@cluster0.nbpu5rm.mongodb.net/?retryWrites=true&w=majority";
+const db = 'mongodb+srv://roskichuk:qwerty12345@cluster0.nbpu5rm.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose
 .connect(db)
 .then(() => {
-    console.log("DB Strat")
+    console.log('DB Strat')
 })
 app.use(cors());
 app.use(express.json())
 
-app.post("/create-shower",ShowerCabinController.create);
-app.post("/create-standart-mirrors",MirrorsStandartController.create);
-app.get("/get-all-shower",ShowerCabinController.getAll);
-app.get("/get-all-standart-mirrors",MirrorsStandartController.getAll);
+app.post('/create-shower',ShowerCabinController.create);
+app.post('/create-standart-mirrors',MirrorsStandartController.create);
+app.get('/get-all-shower',ShowerCabinController.getAll);
+app.get('/get-all-standart-mirrors',MirrorsStandartController.getAll);
 
 app.patch('/update-goods',MirrorsStandartController.updateGoods)
 app.patch('/update-type',MirrorsStandartController.updateType)
 
 app.listen(process.env.PORT,() => {
-    console.log("server start",process.env.PORT)
+    console.log('server start',process.env.PORT)
 })
