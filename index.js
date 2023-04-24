8e0623f2e22672f2807597326dded1b19f21267c
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-04-24 12:25:24 +0300

    renage path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import * as ShowerCabinController from './controller/showerCabinController.js';
+import * as ShowerCabinController from './controller/ShowerCabinController.js';
 import * as MirrorsStandartController from './controller/MirrorsStandartController.js';
 
 dotenv.config();
