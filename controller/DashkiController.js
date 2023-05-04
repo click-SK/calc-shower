30f1e2a5700909be8856545a3d87c9a9fa89049e
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-05-04 15:21:34 +0300

    add dashki furniture path

--- controller/DashkiController.js
+++ controller/DashkiController.js
@@ -1,4 +1,28 @@
 import Dashki from '../models/Dashki.js';
+import multer from 'multer';
+import cloudinary from 'cloudinary';
+import fs from 'fs';
+
+// конфігуруємо Cloudinary
+cloudinary.config({
+  cloud_name: 'dzroxyus8',
+  api_key: '235818666177812',
+  api_secret: '8-Mw7cej-V9GD1d8oPZ8d3djEgo'
+});
+
+const storage = multer.diskStorage({
+destination: (_, __, cb) => {
+  if (!fs.existsSync('uploads')) {
+    fs.mkdirSync('uploads');
+  }
+  cb(null, 'uploads');
+},
+filename: (_, file, cb) => {
+  cb(null, file.originalname);
+},
+});
+
+export const upload = multer({ storage });
 
 export const create = async (req, res) => {
     try{
@@ -167,3 +191,178 @@ export const updateDashkiSize = async (req,res) => {
         console.log(e);
     }
 }
+
+//-------------
+
+export const addFurniture = async (req,res) => {
+  try {
+    const {showerId} = req.body;
+    const furniture = {
+      "count": 1,
+      "mainImage": "url",
+      "title": "Назва",
+      "drawingImg": "url",
+      "depends": [],
+      "colorsFurniture": [
+      ]
+    };
+    const showerCabin = await Dashki.findOneAndUpdate(
+      { _id: showerId },
+      { $push: { furniture: furniture } },
+      { new: true }
+    );
+    res.json(showerCabin)
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const changeFurnitureColors = async (req, res) => {
+  const {color, price, showerCabinId, furnitureId, currentId} = req.body;
+  try {
+    const colorsFurniture = {color: color, price: price};
+
+    const showerCabin = await Dashki.findOneAndUpdate(
+      { _id: showerCabinId, "furniture._id": furnitureId },
+      { $set: { "furniture.$[outer].colorsFurniture.$[inner]": colorsFurniture } },
+      { new: true, arrayFilters: [{ "outer._id": furnitureId }, { "inner._id": currentId }] }
+    );
+
+    await res.json(showerCabin);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateDashkiCabinFurnitureDepends = async (req, res) => {
+  const {showerCabinId, colors, furnitureId, idx} = req.body;
+  console.log('showerCabinId',showerCabinId);
+  console.log('furnitureId',furnitureId);
+  try {
+    const showerCabin = await Dashki.findById(showerCabinId);
+    showerCabin.furniture[idx].depends = colors;
+    const updatedShowerCabin = await showerCabin.save();
+    await res.json(updatedShowerCabin);
+
+  } catch (err) {
+    console.error(err);
+    throw new Error('Failed to update shower cabin colors');
+  }
+}
+
+export const updateDashkiCabinFurnitureMainImage = async (req,res) => {
+  try {
+    const {furnitureId, showerId} = req.body;
+
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
+
+    const shower = await Dashki.findOneAndUpdate(
+      { _id: showerId, "furniture._id": furnitureId}, // умовний оператор
+      { $set: { "furniture.$[outer].mainImage": result.secure_url } },
+      { new: true, arrayFilters: [{ "outer._id": furnitureId }] } 
+    );
+
+    res.json(shower)
+
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+export const updateDashkiCabinFurnitureSecondImage = async (req,res) => {
+  try {
+    const {furnitureId, showerId} = req.body;
+
+    const result = await cloudinary.v2.uploader.upload(req.file.path);
+
+    const shower = await Dashki.findOneAndUpdate(
+      { _id: showerId, "furniture._id": furnitureId}, // умовний оператор
+      { $set: { "furniture.$[outer].drawingImg": result.secure_url } },
+      { new: true, arrayFilters: [{ "outer._id": furnitureId }] } 
+    );
+
+    res.json(shower)
+
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+export const updateDashkiCabinFurnitureTitle = async (req,res) => {
+  try {
+    const {furnitureId, showerId, title} = req.body;
+
+    const shower = await Dashki.findOneAndUpdate(
+      { _id: showerId, "furniture._id": furnitureId}, // умовний оператор
+      { $set: { "furniture.$[outer].title": title } },
+      { new: true, arrayFilters: [{ "outer._id": furnitureId }] } 
+    );
+    res.json(shower)
+
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+export const addNewFurnitureColors = async (req,res) => {
+  const { showerId, furnitureId, color, price } = req.body;
+  try {
+    const showerCabin = await Dashki.findOneAndUpdate(
+      { _id: showerId, "furniture._id": furnitureId },
+      { $push: { "furniture.$.colorsFurniture": { color: color, price: price } } },
+      { new: true }
+    );
+
+    await res.json(showerCabin);
+  } catch (err) {
+    console.error(err);
+    throw new Error('Failed to add color to furniture');
+  }
+}
+
+export const removeFurnitureColors = async (req, res) => {
+  try {
+    const { showerId, furnitureId, currentId } = req.body;
+
+    const shower = await Dashki.findByIdAndUpdate(
+      showerId,
+      {
+        $pull: { 
+          "furniture.$[furniture].colorsFurniture": { _id: currentId } 
+        }
+      },
+      {
+        new: true,
+        arrayFilters: [
+          { "furniture._id": furnitureId }
+        ]
+      }
+    );
+
+    res.json(shower);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Failed to remove color from furniture" });
+  }
+}
+
+export const removeDashkiFurniture = async (req, res) => {
+  try {
+    const { showerId, furnitureId } = req.body;
+
+    const shower = await Dashki.findOneAndUpdate(
+      { _id: showerId },
+      { $pull: { furniture: { _id: furnitureId } } },
+      { new: true }
+    );
+
+    if (!shower) {
+      return res.status(404).json({ message: 'Shower cabin not found' });
+    }
+
+    return res.json(shower);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: 'Failed to remove shower furniture' });
+  }
+};
--- router/DashkiRouter.js
+++ router/DashkiRouter.js
@@ -11,4 +11,15 @@ router.patch('/update-dashki-type',DashkiController.updateDashkiType);
 router.patch('/update-dashki-size',DashkiController.updateDashkiSize);
 router.delete('/remove-dashki-color',DashkiController.removeDashkiColor);
 router.patch('/update-dashki-color',DashkiController.updateDashkiColor);
+
+router.patch('/add-furniture',DashkiController.addFurniture);
+router.patch('/update-furniture-color',DashkiController.changeFurnitureColors);
+
+router.patch('/update-dashki-furniture-depends',DashkiController.updateDashkiCabinFurnitureDepends);
+router.patch('/update-dashki-furniture-main-image',DashkiController.upload.single('mainImage'),DashkiController.updateDashkiCabinFurnitureMainImage);
+router.patch('/update-dashki-furniture-second-image',DashkiController.upload.single('drawingImg'),DashkiController.updateDashkiCabinFurnitureSecondImage);
+router.patch('/update-dashki-furniture-title',DashkiController.updateDashkiCabinFurnitureTitle);
+router.patch('/add-new-dashki-furniture-colors',DashkiController.addNewFurnitureColors);
+router.delete('/remove-dashki-furniture-colors',DashkiController.removeFurnitureColors);
+router.delete('/remove-dashki-furniture',DashkiController.removeDashkiFurniture);
 export default router;
